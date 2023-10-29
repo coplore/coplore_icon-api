df02492ec63e0716f006ef7d33953f364c84aff4
chan9yu <wry5533@gmail.com>
2023-10-29 16:23:23 +0900

    fix: coin symbol 대문자 구별 못하는 부분 수정

--- src/icons/icons.service.ts
+++ src/icons/icons.service.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 @Injectable()
 export class IconsService {
 	getIcon(symbol: string): StreamableFile {
-		let filePath = path.join(__dirname, `../../public/icons/${symbol}.png`);
+		let filePath = path.join(__dirname, `../../public/icons/${symbol.toLowerCase()}.png`);
 		if (!fs.existsSync(filePath)) {
 			filePath = path.join(__dirname, '../../public/icons/_no_image_.png');
 		}
