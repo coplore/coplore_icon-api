ceee04e26a1278a57aeb4d89c65bbeebf4b175a9
chan9yu <wry5533@gmail.com>
2023-10-29 16:25:38 +0900

    feat: 정적 파일 서빙 기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IconsModule } from './icons/icons.module';
 
 @Module({
-	imports: [IconsModule],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: path.join(__dirname, '..', 'public'),
+			exclude: ['/api*']
+		}),
+		IconsModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
 
 import { AppModule } from './app.module';
 
+const API_PREFIX = 'api' as const;
+const PORT = process.env.PORT || 3065;
+
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(3065);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+	app.setGlobalPrefix(API_PREFIX);
+	app.useStaticAssets(path.join(__dirname, '..', 'public'));
+	app.setViewEngine('html');
+
+	await app.listen(PORT);
+	Logger.log(`🚀 Coplore Icon API Server Start: http://localhost:${PORT}/${API_PREFIX}`);
 }
+
 bootstrap();
